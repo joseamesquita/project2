62da0ca6c5add3339aa404a270ceffd438fb341c
Jose Amesquita <joseamesquita11@gmail.com>
2020-01-17 09:05:57 -0800

    1. add scripts file 2. include branching 3. add variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    $("form#celebrityForm").submit(function (event) {
+        var days = $("input:radio[name=days]:checked").val();
+        var person = $("input:radio[name=person]:checked").val();
+        var brand = $("input:radio[name=brand]:checked").val();
+
+
+        if (days === "1" && person === "1" && brand === "3") {
+            $("#two, #three").hide();
+            $("#one").show();
+        } else if (days === "4" && person === "2" && brand === "2") {
+            $("#one, #three").hide();
+            $("#two").show();
+        } else if (days === "3" && person === "1" && brand === "4") {
+            $("#one, #two").hide();
+            $("#three").show();
+        }
+
+        event.preventDefault();
+    });
 
 });
