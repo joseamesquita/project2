060a625f0bf70b90755946147e5f08b76e2ad5d7
Jose Amesquita <joseamesquita11@gmail.com>
2020-01-17 10:16:24 -0800

    add three survey questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     $("form#programming-language").submit(function (event) {
-        var days = $("input:radio[name=days]:checked").val();
+        var code = $("input:radio[name=code]:checked").val();
         var person = $("input:radio[name=person]:checked").val();
-        var brand = $("input:radio[name=brand]:checked").val();
+        var app = $("input:radio[name=app]:checked").val();
 
 
-        if (days === "1" && person === "1" && brand === "3") {
+        if (code === "1" && person === "1" && app === "2") {
             $("#two, #three").hide();
             $("#one").show();
-        } else if (days === "4" && person === "2" && brand === "2") {
+        } else if (code === "4" && person === "2" && app === "2") {
             $("#one, #three").hide();
             $("#two").show();
-        } else if (days === "3" && person === "1" && brand === "4") {
+        } else if (code === "3" && person === "1" && app === "1") {
             $("#one, #two").hide();
             $("#three").show();
         }
